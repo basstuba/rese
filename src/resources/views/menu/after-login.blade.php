2b9fec3a5195532e6aa0b7aeaa6a004c355ff148
nutka <tuba4080@outlook.com>
2024-08-15 22:07:10 +0200

    viewのstyleの修正

--- src/resources/views/menu/after-login.blade.php
+++ src/resources/views/menu/after-login.blade.php
@@ -2,21 +2,21 @@
     <div class="modal-back">
         <a class="modal-back__button" href="#">&times;</a>
     </div>
-    <div class="menu-content">
-        <div class="menu-item">
+    <div class="menu-content__after">
+        <div class="menu-item__after">
             <a class="menu-item__link" href="{{ route('index') }}">Home</a>
         </div>
-        <div class="menu-item">
+        <div class="menu-item__after">
             <form class="logout-form" action="/logout" method="post">
                 @csrf
                 <button class="logout-button" type="submit">Logout</button>
             </form>
         </div>
-        <div class="menu-item">
+        <div class="menu-item__after">
             <a class="menu-item__link" href="{{ route('linkUser') }}">Mypage</a>
         </div>
         @if(isset($user) && $user['role'] == 'company')
-        <div class="menu-item">
+        <div class="menu-item__after">
             <a class="menu-item__link" href="{{ route('multiIndex') }}">ManagementLogin</a>
         </div>
         @endif
--- src/resources/views/menu/before-login.blade.php
+++ src/resources/views/menu/before-login.blade.php
@@ -2,14 +2,14 @@
     <div class="modal-back">
         <a class="modal-back__button" href="#">&times;</a>
     </div>
-    <div class="menu-content">
-        <div class="menu-item">
+    <div class="menu-content__before">
+        <div class="menu-item__before">
             <a class="menu-item__link" href="{{ route('index') }}">Home</a>
         </div>
-        <div class="menu-item">
+        <div class="menu-item__before">
             <a class="menu-item__link" href="{{ route('linkRegister') }}">Registration</a>
         </div>
-        <div class="menu-item">
+        <div class="menu-item__before">
             <a class="menu-item__link" href="{{ route('linkLogin') }}">Login</a>
         </div>
     </div>
